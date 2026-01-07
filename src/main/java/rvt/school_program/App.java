aff4fb51939f901964dbae6d72d01d5bab471eb6
KirilDev <alexprogrammer2023@gmail.com>
2026-01-07 16:05:26 +0200

    Students and Teachers object created

--- src/main/java/rvt/school_program/App.java
+++ src/main/java/rvt/school_program/App.java
@@ -5,6 +5,10 @@ public class App {
         Person personFirst = new Person("Kirill", "N/A", "Krišjana Valdemara", "students@rvt.lv");
         personFirst.getName();
 
+        Student studentFirst = new Student("Nikita", "N/A", "Krišjana Valdemara", "nikita.student@rvt.lv", "Engineering", 1, 100);
+        studentFirst.getName();
+
+        Teacher teacherFirst = new Teacher(null, null, null, null, null)
         // personFirst.name = "Alex"; | Doesn't work because person field isn't visible.
     }
 }
--- src/main/java/rvt/school_program/Student.java
+++ src/main/java/rvt/school_program/Student.java
@@ -3,9 +3,13 @@ package rvt.school_program;
 public class Student extends Person{
 
     private String course;
-    public Student(String name, String surname, String address, String email, String course) {
+    private int courseNumber;
+    private int credit;
+    public Student(String name, String surname, String address, String email, String course, int courseNumber, int credit) {
         super(name, surname, address, email);
         this.course = course;
+        this.courseNumber = courseNumber;
+        this.credit = credit;
     }
     
 }

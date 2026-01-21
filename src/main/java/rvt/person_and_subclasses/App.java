bf1e21f304b0ff9cbf624c8d92248554ed6d8bdc
KirilDev <alexprogrammer2023@gmail.com>
2026-01-21 17:25:02 +0200

    Two new persons added

--- src/main/java/rvt/person_and_subclasses/App.java
+++ src/main/java/rvt/person_and_subclasses/App.java
@@ -4,18 +4,25 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args){
+        Student kirill = new Student("Kirill", "private");
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
         ollie.study();
         ollie.study();
         ollie.study();
 
+        kirill.study();
+        kirill.study();
+
         Teacher Ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
         Teacher Esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 4800);
+        Teacher Origo = new Teacher("Origo", "Krisjana street", 1300);
 
         ArrayList<Person> persons = new ArrayList<Person>();
         persons.add(Ada);
         persons.add(Esko);
         persons.add(ollie);
+        persons.add(kirill);
+        persons.add(Origo);
 
         printPersons(persons);
 

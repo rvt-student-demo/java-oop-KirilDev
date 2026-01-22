c838862501d5f301d586c9d5da9da9b8e583d03c
KirilDev <alexprogrammer2023@gmail.com>
2026-01-22 13:01:29 +0200

    Chapter 100 exception example

---
+++ src/main/java/rvt/exceptions/SquareNum.java
@@ -0,0 +1,24 @@
+package rvt.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SquareNum {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        System.out.println("Enter a number: ");
+
+        try {
+            number = scanner.nextInt();
+            System.out.println("Square of the number is " + number * number);
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Wrong data...");
+            System.out.println("Try again!");
+            main(null);
+        }
+        System.out.println("Shutdown...");
+    }
+}

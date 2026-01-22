4b1353f6f4a739ef0a377d2975268427947a3053
KirilDev <alexprogrammer2023@gmail.com>
2026-01-22 13:28:46 +0200

    Chapter 100 - Exercise 1 finished

--- src/main/java/rvt/exceptions/DivisionPractice.java
+++ src/main/java/rvt/exceptions/DivisionPractice.java
@@ -1,8 +1,61 @@
 package rvt.exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class DivisionPractice {
     public static void main(String[] args){
-        
+        Scanner scan = new Scanner(System.in);
+        Double numberator;
+        Double divisor;
+
+        while (true) {
+            try {
+                System.out.println("Enter the numerator: ");
+
+                String numeratorInput = scan.nextLine().trim();
+                if (numeratorInput.equalsIgnoreCase("quit")) {
+                    System.out.println(" ");
+                    break;
+                }
+
+                numberator = Double.parseDouble(numeratorInput);
+
+               System.out.println("Enter the divisor: ");
+               String divisorInput = scan.nextLine().trim();
+               divisor = Double.parseDouble(divisorInput);
+
+                if ( divisor == 0 ) throw new ArithmeticException();
+
+               System.out.println(numberator + " / " + divisor + " is " + numberator / divisor);
+            } catch (InputMismatchException e) {
+                System.out.println("You entered bad data.");
+                System.out.println("Please try again.");
+                scan.nextLine();
+            } catch (ArithmeticException e){
+              System.out.println("You can't divide by zero!!!");  
+            } 
+        }
+    
     }
     
 }
+
+
+/*
+requirments:
+Make a program: 
+Enter the numerator: 12
+Enter the divisor:  4
+12 / 4 is 3
+
+Enter the numerator: 12
+Enter the divisor  : 0
+You can't divide 12 by 0
+
+Enter the numerator: glarch
+You entered bad data.
+Please try again.
+
+Enter the numerator: quit
+*/
